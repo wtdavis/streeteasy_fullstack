95cc7afc40ce873d1f9a5a8a83a53ed059445395
wtdavis <wtdavis@wesleyan.edu>
2023-04-03 14:51:52 -0400

    renderingss

--- db/migrate/20230403173927_listings.rb
+++ db/migrate/20230403173927_listings.rb
@@ -1,9 +1,10 @@
 class Listings < ActiveRecord::Migration[7.0]
   def change
     create_table :listings do |t|
-    t.string :location, null: false
-    t.string :description
-    t.string :lister_id, null: false
-    t.string :type, null: false
+      t.string :location, null: false
+      t.string :description
+      t.string :lister_id, null: false
+      t.string :type, null: false
+    end
   end
 end
