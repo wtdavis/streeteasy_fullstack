c70a686e56252b8ee366c31d8f25487a03c817ac
wtdavis <wtdavis@wesleyan.edu>
2023-04-03 14:42:51 -0400

    renderings

--- db/migrate/20230403173927_listings.rb
+++ db/migrate/20230403173927_listings.rb
@@ -3,7 +3,7 @@ class Listings < ActiveRecord::Migration[7.0]
     t.string :location, null: false
     t.string :description
     t.string :lister_id, null: false
-    t.string, :type, null: false
+    t.string :type, null: false
     
   end
 end
