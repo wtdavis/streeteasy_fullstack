0652a235283d8779847ab116fffefd2e9c8d4064
wtdavis <wtdavis@wesleyan.edu>
2024-01-24 20:41:58 -0500

    refine splash prefereneces tile styling

--- frontend/src/components/SplashSectionMain/SplashSub3.js
+++ frontend/src/components/SplashSectionMain/SplashSub3.js
@@ -3,6 +3,9 @@ import SplashTileElevator from "./SplashTiles/SplashTileElevator"
 import SplashTilePets from "./SplashTiles/SplashTilePets"
 
 function SplashSubSection3 () {
+    let a = Math.floor(Math.random() * 45)
+    let b = Math.floor(Math.random() * 45)
+
     return(
          <>
             <div id="splashsub3" className="splashsection">
@@ -11,7 +14,11 @@ function SplashSubSection3 () {
                     <SplashTileElevator/>
                     <SplashTilePets/>
                 </div>
-            <div className="splashtilebutton splashsub3button">search button</div>
+                <div className="splashbuttonrow">
+                    <div className="splashsub3button">Search {a} Rentals</div>
+                    <div className="splashsub3button">Search {b} Sales</div>
+
+                </div>
             </div>
 
   
